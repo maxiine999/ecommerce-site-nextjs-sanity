fcae7ee7e3f2f9a676d8f2e2c2a931de1907699c
maxiine999 <139379941+maxiine999@users.noreply.github.com>
2024-07-30 16:53:10 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
-import Link from 'next/link'
-import { AiOutlineShopping } from 'react-icons/ai'
-import Cart from './Cart'
-import { useStateContext } from '@/context/StateContext'
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
+import Cart from './Cart';
+import { useStateContext } from '@/context/StateContext';
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';  // Import Clerk components
+import CustomSignInButton from '@/components/CustomSignIn';  // Import the CustomSignInButton
+
 const Navbar = () => {
-  const {showCart,setShowCart,totalQuantities}=useStateContext();
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
   return (
     <div className='navbar-container'>
+      <div className='navbar-left'>
+        <SignedOut>
+          <CustomSignInButton />  {/* Sign-In Button for Signed Out users */}
+        </SignedOut>
+        <SignedIn>
+          <UserButton />  {/* User Button for Signed In users */}
+        </SignedIn>
+      </div>
       <p className='logo'>
-        <Link href="/">AttireAvenue
-        </Link>
+        <Link href="/">AttireAvenue</Link>
       </p> 
-      <button type="button" className='cart-icon' onClick={()=>setShowCart(true)}>
-      <AiOutlineShopping/>
+      <button type="button" className='cart-icon' onClick={() => setShowCart(true)}>
+        <AiOutlineShopping />
         <span className='cart-item-qty'>
           {totalQuantities}
         </span>
       </button>
-      {showCart && <Cart/>}
-      
+      {showCart && <Cart />}
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
